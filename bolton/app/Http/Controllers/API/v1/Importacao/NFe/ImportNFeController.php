a1a942707d0c94dba89a530d64488a67eb4a7465
Cesar Pierobom <cesar_pierobom@outlook.com>
2019-07-02 10:47:53 -0300

    adicionado o controller de importacao de nfes da Arquivei API

---
+++ bolton/app/Http/Controllers/API/v1/Importacao/NFe/ImportNFeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\API\v1\Importacao\NFe;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Importacao\NFe\ImportNFeRequest;
+
+class ImportNFeController extends Controller
+{
+    /**
+     * Import several NFes from Arquivei's API
+     *
+     * @param  \App\Http\Requests\Importacao\NFe\ImportNFeRequest  $request
+     */
+    public function import(ImportNFeRequest $request)
+    {
+        
+    }
+}
